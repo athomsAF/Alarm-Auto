e4eb3ceb013e926919a03c014479ae9a00a0aee6
athomsAF <thomas.drddr03@gmail.com>
2022-08-09 13:37:21 +0200

    liste de tous les debuts des événements

--- Calendrier.py
+++ Calendrier.py
@@ -3,13 +3,14 @@ import requests
 
 url = "https://outlook.office365.com/owa/calendar/d7749e57c7964bc1ae8406ac11ff6f10@edu.devinci.fr/8c71d34af3f849e0b1a6dfc66c17d87714175672858623324700/calendar.ics"
 c = Calendar(requests.get(url).text)
-event=str(c.events)
-disallowed_characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZéàè': ’+'`-?_&{.<[]>"
+print(c.events)
+
+"""disallowed_characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZéàè': ’+'`-?_&{.<[]>"
 for character in disallowed_characters:
 	event = event.replace(character, "")
 
 #event=filter(str.isalpha,c.events)
-L=event.split(",")
+L=event.split(",")  
 j=0
 save=""
 for element in L:
@@ -28,4 +29,4 @@ N=[int(L[1])]
 for i in range(1,len(L)):
     if (L[i][5:8]!=L[i-1][5:8]):
         N=N+[int(L[i])]
-print(N)
+print(N)"""
